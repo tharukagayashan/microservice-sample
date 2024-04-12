cd308b7627b107f65b60e9268fe6350891b2fcd5
Tharuka Gayashan <tharuka.gayashan.5@gmail.com>
2024-04-13 01:02:51 +0530

    Refactored code

--- course-center/src/test/java/com/projects/coursecenter/CourseCenterApplicationTests.java
+++ course-center/src/test/java/com/projects/coursecenter/CourseCenterApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CourseCenterApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
--- payment-center/src/main/java/com/projects/paymentcenter/PaymentCenterApplication.java
+++ payment-center/src/main/java/com/projects/paymentcenter/PaymentCenterApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PaymentCenterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaymentCenterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentCenterApplication.class, args);
+    }
 
 }
