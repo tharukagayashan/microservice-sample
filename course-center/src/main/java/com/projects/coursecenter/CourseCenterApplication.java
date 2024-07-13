f6788532ea5e425e2c07e7632cdea0d40c2e03c7
Tharuka Gayashan <tharuka.gayashan.5@gmail.com>
2024-07-13 14:22:07 +0530

    Add command line inputs to DBs

--- course-center/src/main/java/com/projects/coursecenter/CourseCenterApplication.java
+++ course-center/src/main/java/com/projects/coursecenter/CourseCenterApplication.java
@@ -16,55 +16,55 @@ public class CourseCenterApplication {
         SpringApplication.run(CourseCenterApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner loadCourseData(CourseRepository courseRepository) {
-        return (args) -> {
-
-            if (courseRepository.count() == 0) {
-                courseRepository.save(new Course(
-                                0L,
-                                "Java Programming",
-                                50000.00,
-                                12,
-                                "Learn Java Programming from scratch",
-                                "Beginner",
-                                "Active"
-                        )
-                );
-
-                courseRepository.save(new Course(
-                                0L,
-                                "Spring Boot",
-                                60000.00,
-                                8,
-                                "Learn Spring Boot from scratch",
-                                "Intermediate",
-                                "Active"
-                        )
-                );
-
-                courseRepository.save(new Course(
-                                0L,
-                                "React JS",
-                                40000.00,
-                                10,
-                                "Learn React JS from scratch",
-                                "Intermediate",
-                                "Active"
-                        )
-                );
-
-                courseRepository.save(new Course(
-                                0L,
-                                "Angular",
-                                45000.00,
-                                10,
-                                "Learn Angular from scratch",
-                                "Intermediate",
-                                "Active"
-                        )
-                );
-            }
-        };
-    }
+//    @Bean
+//    public CommandLineRunner loadCourseData(CourseRepository courseRepository) {
+//        return (args) -> {
+//
+//            if (courseRepository.count() == 0) {
+//                courseRepository.save(new Course(
+//                                0L,
+//                                "Java Programming",
+//                                50000.00,
+//                                12,
+//                                "Learn Java Programming from scratch",
+//                                "Beginner",
+//                                "Active"
+//                        )
+//                );
+//
+//                courseRepository.save(new Course(
+//                                0L,
+//                                "Spring Boot",
+//                                60000.00,
+//                                8,
+//                                "Learn Spring Boot from scratch",
+//                                "Intermediate",
+//                                "Active"
+//                        )
+//                );
+//
+//                courseRepository.save(new Course(
+//                                0L,
+//                                "React JS",
+//                                40000.00,
+//                                10,
+//                                "Learn React JS from scratch",
+//                                "Intermediate",
+//                                "Active"
+//                        )
+//                );
+//
+//                courseRepository.save(new Course(
+//                                0L,
+//                                "Angular",
+//                                45000.00,
+//                                10,
+//                                "Learn Angular from scratch",
+//                                "Intermediate",
+//                                "Active"
+//                        )
+//                );
+//            }
+//        };
+//    }
 }
